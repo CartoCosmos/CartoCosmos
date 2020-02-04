c3cc2edda5c4eb2c5bbc343a4ab4dd1508151dfb
Scott Ames <scottaames@gmail.com>
2020-02-03 19:21:06 -0700

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import L from 'leaflet';
-import L.Proj from 'proj4leaflet';
-
-export * from './LayerCollection';
-export * from './PlanetaryLayer';
-export * from './Projection';
-
-console.log("hello world!");
