1d357e00de7968450dbe6b03f3cd1cf5bdd6e64f
cgm78 <42878615+cgm78@users.noreply.github.com>
2020-02-21 17:16:12 -0700

    Updating files with new placement of server query
    working on getting display to work.

--- AutoComplete/customIndex.js
+++ AutoComplete/customIndex.js
@@ -1,17 +1,5 @@
 //define("require");
 //require(['@tarekraafat/autocomplete.js/dist/js/autoComplete']);
-function loadGeoJson(data) {
-    console.log(data);
-    console.log("We are here, autocomplete is empty");
-    var nameArray = [];
-    var index = 1840;
-    while (JSON.parse(JSON.stringify(data)).features[index] != null){
-        console.log(JSON.parse(JSON.stringify(data)).features[index].name);
-        nameArray.push(JSON.parse(JSON.stringify(data)).features[index].name);
-        index++;
-         }
-         return nameArray;
-        }
 new autoComplete({
     data: {                              // Data src [Array, Function, Async] | (REQUIRED)
       src: async () => {
@@ -24,18 +12,11 @@ new autoComplete({
         // Format data into JSON
         //const data = await source.json();
         // Return Fetched data
-        //return source;     
-        var nameArray = [];
-        $.ajax({
-            url: "https://astrocloud.wr.usgs.gov/dataset/data/nomenclature/MARS/WFS?version=1.1.0.0&service=WFS&request=GetFeature&srsname=EPSG4326&outputFormat=application/json",
-            dataType: 'json',
-            success: nameArray = loadGeoJson,
-            timeout : 320000
-            });
-            console.log(nameArray[0]);
-            return nameArray;
+        //return source.features[0].name;    
+        // console.log(nameArray[0]); 
+        return nameArray;
       },
-      key: ["title"],
+      key: ["name"],
       cache: false
     },
     /*query: {                               // Query Interceptor               | (Optional)
@@ -52,7 +33,7 @@ new autoComplete({
     selector: "#autoComplete",          
     threshold: 3,                       
     debounce: 300,                       
-    searchEngine: "loose",              
+    searchEngine: "strict",              
     resultsList: {                       
         container: source => {
             source.setAttribute("id", "nomenclature");
@@ -77,6 +58,6 @@ new autoComplete({
         document.querySelector("#autoComplete_list").appendChild(result);
     },
     onSelection: feedback => {             // Action script onSelection event | (Optional)
-        console.log(feedback.selection.value.image_url);
+        console.log(feedback.toString());
     }
 });
