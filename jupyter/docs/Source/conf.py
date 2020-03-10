eaec36c2c4966c8c6bb09b394a7af7ea304b977c
Jacob Kaufman <jmk478@nau.edu>
2020-03-09 22:05:00 -0700

    Update conf.py

--- jupyter/docs/Source/conf.py
+++ jupyter/docs/Source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.append('/Users/jkaufy/Desktop/Pydocs/sphinx/scripts')
+sys.path.append('/CartoComos-1/jupyter/src')
 
 # sys.path.insert(0, os.path.abspath('.'))
 
