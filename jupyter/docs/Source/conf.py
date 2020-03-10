36f1762a146da57b3d49c4aef2ac90ac4b16b105
Jacob Kaufman <jmk478@nau.edu>
2020-03-09 22:43:54 -0700

    Update conf.py

--- jupyter/docs/Source/conf.py
+++ jupyter/docs/Source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.append('/jupyter/src/')
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../CartoCosmos-1/jupyter/src'))
+sys.path.insert(0, os.path.abspath('../../CartoCosmos-1/jupyter/src/'))
 #sys.path.insert(0, os.path.abspath('../../src'))
 
 #extensions = [
