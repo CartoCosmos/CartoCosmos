c2bfd52355ee7d3d854678914431376c409e1438
Jacob Kaufman <jmk478@nau.edu>
2020-03-09 22:49:15 -0700

    Update conf.py

--- jupyter/docs/Source/conf.py
+++ jupyter/docs/Source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.append('/jupyter/src/')
+sys.path.append('../../jupyter/src/')
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../CartoCosmos-1/jupyter/src/'))
-#sys.path.insert(0, os.path.abspath('../../src'))
+#sys.path.insert(0, os.path.abspath('../../CartoCosmos-1/jupyter/src/'))
+#sys.path.insert(0, os.path.abspath('../../jupyter/src/'))
 
 #extensions = [
 #             'jupyter_sphinx',
