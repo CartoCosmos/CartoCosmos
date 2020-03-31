7b7ab80c16310eff37cb0e0192db4e512c0f2fcd
Scott Ames <scottaames@gmail.com>
2020-03-31 13:03:47 -0700

    created search box above map

---
+++ app/src/components/presentational/AutoCompleteInput.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+import { makeStyles, withStyles, fade } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    textAlign: "center",
+    maxHeight: 80,
+    height: 80,
+    width: 500
+    //overflow: "hidden"
+  },
+  container: {
+    padding: "1rem",
+    height: 50,
+    width: "90%",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    margin: "auto"
+  },
+  textbox: {
+    flex: "3 1 auto",
+    backgroundColor: "#e9ecef",
+    "&:focus": {
+      borderColor: "#1971c2"
+    }
+  },
+  button: {
+    height: 40,
+    color: "#fff",
+    backgroundColor: "#1971c2",
+    width: "6rem",
+    marginLeft: "1rem",
+    alignSelf: "center",
+    "&:hover": {
+      backgroundColor: fade("#1971c2", 0.7)
+    }
+  }
+}));
+
+/**
+ * Custom Component that uses Tooltip with modified css styling
+ */
+const StyledTooltip = withStyles(theme => ({
+  tooltip: {
+    backgroundColor: "#f5f5f9",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 250,
+    fontSize: 12,
+    border: "1px solid #dadde9"
+  }
+}))(Tooltip);
+
+export default function AutoCompleteInput() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <StyledTooltip
+        title={
+          <Typography variant="subtitle1">
+            Enter the name of a surface feature and select it to open a web page
+            with information about that feature.
+          </Typography>
+        }
+        enterDelay={800}
+        leaveDelay={250}
+        arrow
+        TransitionComponent={Zoom}
+      >
+        <div className={classes.container}>
+          <TextField
+            className={classes.textbox}
+            variant="outlined"
+            label="Enter Surface Feature Name"
+            InputLabelProps={{
+              shrink: true
+            }}
+            id="autoComplete"
+            tabIndex="1"
+            onfocusout="unrenderBox()"
+            onfocusin="renderBox()"
+          />
+          <Button variant="contained" className={classes.button} id="wktButton">
+            Search
+          </Button>
+        </div>
+      </StyledTooltip>
+    </div>
+  );
+}
---
+++ app/src/js/autocomplete/render.js
@@ -0,0 +1,6 @@
+function unrenderBox(){
+    document.getElementById("#autoComplete_list").style.visibility = "hidden";
+}
+function renderBox(){
+    document.getElementById("#autoComplete_list").style.visibility = "visible";
+}
