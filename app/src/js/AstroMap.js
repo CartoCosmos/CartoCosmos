3ed6b784e5b3abcc430d0002c95cd5da0d1a932b
Scott Ames <scottaames@gmail.com>
2020-05-04 09:45:36 -0700

    edited comments

--- app/src/js/AstroMap.js
+++ app/src/js/AstroMap.js
@@ -269,7 +269,7 @@ export default L.Map.AstroMap = L.Map.extend({
   },
 
   /**
-   * @function AstroMap.prototype.getCenter
+   * @function AstroMap.prototype.center
    * @description getter method to access the center of the map.
    *
    * @return {LatLng} The center coordinates of the map.
