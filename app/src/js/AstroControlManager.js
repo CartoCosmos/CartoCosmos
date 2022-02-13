3b0ca7081a4059bde70c81be3a2fbe138c8e464d
Jacob <68128708+jrc632@users.noreply.github.com>
2022-02-12 17:55:36 -0700

    Removed old fullscreen button

--- app/src/js/AstroControlManager.js
+++ app/src/js/AstroControlManager.js
@@ -35,10 +35,6 @@ export default L.AstroControlManager = L.Class.extend({
     this._zoomControl = new L.Control.Zoom();
     this._controls.push(this._zoomControl);
 
-    // Leaflet built-in fullscreen doesn't work for outside controls
-    // this._fullscreenControl = new L.Control.Fullscreen();
-    // this._controls.push(this._fullscreenControl);
-
     this._viewCenterControl = new ViewCenterControl();
     this._controls.push(this._viewCenterControl);
 
